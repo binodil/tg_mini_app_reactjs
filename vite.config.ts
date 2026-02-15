e361c005f677051673f6781815251001ae798936
AbdirayimovS <abdirayimovsardor20@gmail.com>
2026-02-15 19:08:11 +0900

    Update base param with a repo name

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reactjs-template/',
+  base: 'tg_mini_app_reactjs',
   css: {
     preprocessorOptions: {
       scss: {
